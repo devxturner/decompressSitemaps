6022e312abfb7971ae10a9992c849b653f00f681
devxturner <devx.turner@gmail.com>
2025-05-14 17:58:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def decompress_gz():
         return {"error": str(e)}, 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port, debug=True)
